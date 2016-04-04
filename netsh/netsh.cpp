ead64b4fee7d1a7d34f9389c272ad25b71528df1
heat_wave <allpicksucks@gmail.com>
2016-04-05 01:54:01 +0300

    Everything seems to work on netcat.

--- netsh/netsh.cpp
+++ netsh/netsh.cpp
@@ -15,6 +15,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
 #include <sstream>
 #include <vector>
 #include <stdlib.h>
@@ -58,7 +59,7 @@ int fork_pipes(int n, int socket_fd, struct command *cmd) {
     pid_t pid;
     int in, fd[2];
 
-    in = 0;
+    in = socket_fd;
 
     for (i = 0; i < n - 1; ++i) {
         pipe(fd);
@@ -67,8 +68,9 @@ int fork_pipes(int n, int socket_fd, struct command *cmd) {
         in = fd[0];
     }
 
-    if (in != 0)
-        dup2 (in, 0);
+    if (in != 0) {
+        dup2(in, 0);
+    }
     dup2(socket_fd, 1);
 
     /* Execute the last stage with the current process. */
@@ -115,61 +117,6 @@ int create_and_bind(char* port) {
 
     return socket_fd;
 }
-
-//TODO: implement piping multiple commands (leave commented as a stub)
-//    if (listen(socket_fd, 50) == -1) {
-//        error_netsh("Failed to listen");
-//    }
-//
-//    socklen_t client_len = sizeof(client_address);
-//
-//    while (1) {
-//        int client_socket_fd = accept(socket_fd, (struct sockaddr *) &client_address, &client_len);
-//        if (client_socket_fd == -1) {
-//            error_netsh("Failed to accept");
-//        }
-//
-//        char buffer[4096];
-//        memset(&buffer, 0, sizeof(buffer));
-//        size_t read_count = read(client_socket_fd, buffer, sizeof(buffer));
-//        if (read_count == -1) {
-//            error_netsh("Failed to read from socket");
-//        }
-//        if (read_count == sizeof(buffer)) {
-//            //need to eat more input
-//        }
-//        std::string sequence(buffer, read_count - 1);
-//        std::vector<std::string> subcommands = split(sequence, '|');
-//        printf("%d commands\n", subcommands.size());
-//        for (size_t i = 0; i != subcommands.size(); ++i) {
-//            std::vector<std::string> command_args = split(subcommands[i], ' ');
-//            for (std::vector<std::string>::iterator it = command_args.begin(); it != command_args.end();) {
-//                if ((*it).size() == 0)
-//                    it = command_args.erase(it);
-//                else
-//                    ++it;
-//            }
-//            printf("%d\n", command_args.size());
-//            char* argv_to_pass[command_args.size() + 1];
-//            for (size_t j = 0; j != command_args.size(); ++j) {
-//                argv_to_pass[j] = const_cast<char*>(command_args[j].c_str());
-//                printf("%s\n", command_args[j].c_str());
-//            }
-//            argv_to_pass[command_args.size()] = NULL;
-//            char * pointer = (char*)&argv_to_pass;
-//
-//            pid_t pid = fork();
-//
-//            if (pid == -1) {
-//                error_netsh("Failed to fork on input received");
-//            } else if (pid > 0) {
-//                exit(0);
-//            } else {
-//                printf("Executing %s %d \n", argv_to_pass[0], sizeof(argv_to_pass[0]));
-//                execlp(argv_to_pass[0], pointer);
-//            }
-//        }
-//    }
 
 int make_socket_non_blocking (int socket_fd) {
     int flags, s;
@@ -241,7 +188,7 @@ int main(int argc, char *argv[]) {
             }
             event.data.fd = sfd;
             event.events = EPOLLIN | EPOLLET;
-            if (epoll_ctl (efd, EPOLL_CTL_ADD, sfd, &event) == -1) {
+            if (epoll_ctl(efd, EPOLL_CTL_ADD, sfd, &event) == -1) {
                 error_netsh("Epoll_ctl error");
             }
 
@@ -274,7 +221,7 @@ int main(int argc, char *argv[]) {
                             char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
 
                             in_len = sizeof in_addr;
-                            infd = accept (sfd, &in_addr, &in_len);
+                            infd = accept(sfd, &in_addr, &in_len);
                             if (infd == -1) {
                                 if ((errno == EAGAIN) ||
                                     (errno == EWOULDBLOCK)) {
@@ -296,7 +243,7 @@ int main(int argc, char *argv[]) {
 
                             event.data.fd = infd;
                             event.events = EPOLLIN | EPOLLET;
-                            if (epoll_ctl (efd, EPOLL_CTL_ADD, infd, &event) == -1) {
+                            if (epoll_ctl(efd, EPOLL_CTL_ADD, infd, &event) == -1) {
                                 error_netsh("Epoll_ctl error");
                             }
                         }
@@ -304,17 +251,28 @@ int main(int argc, char *argv[]) {
                     }
                     else {
                         /* We have data on the fd waiting to be read. Read and
-                           display it. We must read whatever data is available
+                           use it. We must read whatever data is available
                            completely, as we are running in edge-triggered mode
                            and won't get a notification again for the same
                            data. */
                         int done = 0;
+                        std::vector<struct command> commands;
+                        std::vector<char *> args;
+                        struct command current;
+                        std::string last_arg;
+                        int args_count = 0;
 
-                        while (1) {
+                        int end_of_line = 0;
+                        int is_quote = 0;
+
+                        while (!done) {
                             ssize_t count;
                             char buf[512];
 
-                            count = read (events[i].data.fd, buf, sizeof buf);
+                            int last_space_pos = 0;
+
+                            count = read(events[i].data.fd, buf, sizeof buf);
+                            printf("%d\n", count);
                             if (count == -1) {
                                 /* If errno == EAGAIN, that means we have read all
                                    data. So go back to the main loop. */
@@ -323,31 +281,55 @@ int main(int argc, char *argv[]) {
                                     done = 1;
                                 }
                                 break;
-                            } else if (count == 0) {
-                                /* End of file. The remote has closed the
-                                   connection. */
-                                done = 1;
-                                break;
                             }
 
-                            pid_t pid = fork();
+                            for (int j = 0; j < count; j++) {
+                                if (buf[j] == '\'' && !is_quote) {
+                                    is_quote = 1;
+                                    last_space_pos = j + 1;
+                                    continue;
+                                }
+                                if (buf[j] == '\'' && is_quote || !is_quote && (buf[j] == ' ' || buf[j] == '|' || buf[j] == '\n')) {
+                                    if (j - last_space_pos > 0) {
+                                        last_arg.append(std::string(buf + last_space_pos, j - last_space_pos));
+                                        char * aux = new char[last_arg.size() + 1];
+                                        std::strcpy(aux, last_arg.c_str());
+                                        aux[last_arg.size()] = '\0';
+                                        args.push_back(aux);
+                                        args_count++;
+                                        last_arg = std::string();
+                                    }
+                                    last_space_pos = j + 1;
+                                    is_quote = 0;
+                                }
+                                if (!is_quote && (buf[j] == '|' || buf[j] == '\n')) {
+                                    char ** args_array = new char* [args_count + 1];
+                                    for (int k = 0; k < args_count; ++k) {
+                                        args_array[k] = args[k];
+                                    }
 
-                            if (pid == -1) {
-                                error_netsh("Failed to fork on input received");
-                            } else if (pid > 0) {
-                                // Do nothing
-                            } else {
-                                //TODO: parse a complex command
-                                // this is for testing purposes
-                                const char *ls[] = { "ls", "-l", 0 };
-                                const char *awk[] = { "awk", "{print $1}", 0 };
-                                const char *sort[] = { "sort", 0 };
-                                const char *uniq[] = { "uniq", 0 };
-
-                                struct command cmd [] = { {ls}, {awk}, {sort}, {uniq} };
-
-                                fork_pipes (1, events[i].data.fd, cmd);
+                                    args_array[args_count] = NULL;
+                                    args.clear();
+                                    args_count = 0;
+                                    commands.push_back((struct command) {.argv = (const char **) args_array});
+                                }
+                                if (buf[j] == '\n') {
+                                    done = 1;
+                                    break;
+                                }
                             }
+                            last_arg.append(std::string(buf + last_space_pos, count - last_space_pos));
+                        }
+
+                        pid_t pid = fork();
+
+                        if (pid == -1) {
+                            error_netsh("Failed to fork on input received");
+                        } else if (pid > 0) {
+                            // Do nothing
+                        } else {
+                            printf("%d\n", commands.size());
+                            fork_pipes (commands.size(), events[i].data.fd, &commands[0]);
                         }
 
                         if (done) {
