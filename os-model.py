e88537cd32e08f89dd99486da34862e35beca35b
heat_wave <allpicksucks@gmail.com>
2016-05-03 12:15:42 +0300

    Added pipes, minor cleanup.

--- os-model.py
+++ os-model.py
@@ -13,11 +13,10 @@ class SystemCall(Enum):
 pid_count = 0
 cur_pid = -1
 
-buf = [None] * 200
-
 process_list = []
 file_table = []
 pipe_ends = dict()
+pipe_buffers = dict()
 
 per_process_fdtables = [dict() for i in range(2)]
 
@@ -43,8 +42,13 @@ def close(fildes):
 
 def read(fildes, buffer, count):
     global cur_pid
-    if fildes in pipe_ends:
-        pass
+    if fildes in pipe_ends.keys():
+        offset = 0
+        pipe_buf = pipe_buffers[fildes]
+        while pipe_buf[offset] is not None:
+            buffer[offset] = pipe_buf[offset]
+            pipe_buf[offset] = None
+        return offset
 
     else:
         file_handle = file_table[per_process_fdtables[cur_pid][fildes]]
@@ -58,11 +62,24 @@ def read(fildes, buffer, count):
 
 def write(fildes, buffer, count):
     global cur_pid
-    file_handle = file_table[per_process_fdtables[cur_pid][fildes]]
-    file_handle[0].seek(1, file_handle[2])
-    result = file_handle[0].write(''.join(buffer[:count]))
-    file_table[per_process_fdtables[cur_pid][fildes]] = (file_handle[0], file_handle[1], file_handle[2] + result)
-    return result
+    if fildes in pipe_ends.values():
+        pipe_buf = pipe_buffers[list(pipe_ends.keys())[list(pipe_ends.values()).index(fildes)]]
+        while pipe_buf[len(pipe_buf) - 1] is not None:
+            pass  # block here
+        offset = len(pipe_buf) - 1
+        while offset > 0 and pipe_buf[offset] is not None:
+            offset -= 1
+        offset += 1
+        for i in range(offset, len(pipe_buf)):
+            pipe_buf[i] = buffer[i]
+        return len(pipe_buf) - offset
+
+    else:
+        file_handle = file_table[per_process_fdtables[cur_pid][fildes]]
+        file_handle[0].seek(1, file_handle[2])
+        result = file_handle[0].write(''.join(buffer[:count]))
+        file_table[per_process_fdtables[cur_pid][fildes]] = (file_handle[0], file_handle[1], file_handle[2] + result)
+        return result
 
 
 def dup2(from_fd, to_fd):
@@ -80,6 +97,7 @@ def pipe():
     per_process_fdtables[cur_pid][in_end] = -1
     per_process_fdtables[cur_pid][out_end] = -1
     pipe_ends[in_end] = out_end
+    pipe_buffers[in_end] = [None] * 16
     return in_end, out_end
 
 
@@ -108,31 +126,39 @@ def kernel(program, args, stdin):
         if sys_call == SystemCall.READ:
             read_result = read(*args)
             process_list.append((cont, [read_result], next_pid))
+
         elif sys_call == SystemCall.WRITE:
             write_result = write(*args)
             process_list.append((cont, [write_result], next_pid))
+
         elif sys_call == SystemCall.OPEN:
             open_result = open_fd(*args)
             process_list.append((cont, [open_result], next_pid))
+
         elif sys_call == SystemCall.CLOSE:
             close_result = close(args[0])
             process_list.append((cont, [close_result], next_pid))
+
         elif sys_call == SystemCall.KILL:
             kill_result = kill(args[0])
             process_list.append((cont, [kill_result], next_pid))
+
         elif sys_call == SystemCall.FORK:
             pid_count += 1
             process_list.append((cont, [cur_pid], cur_pid))
             process_list.append((cont, [0], pid_count))
-            per_process_fdtables.append(dict())
+            per_process_fdtables.append(dict())  # imagine that we put stdin/stdout/stderr abstractions here
+
         elif sys_call == SystemCall.EXIT:
-            for fildes in per_process_fdtables[cur_pid].keys():
+            for fildes in list(per_process_fdtables[cur_pid].keys()):
                 close(fildes)
             print("Exit code: {}".format(args[0]))
+
         else:
-            print("ERROR")
+            print("ERROR: no such system call")
 
 
+buf = [None] * 200
 def open_file_0():
     return SystemCall.OPEN, ["/home/heat_wave/.bashrc", "r"], open_file_1
 
@@ -142,7 +168,7 @@ def open_file_1(fildes):
 
 
 def open_file_2(read_count):
-    print(*buf[:read_count])
+    print(''.join(buf[:read_count]))
     return SystemCall.EXIT, [0], None
 
 kernel(open_file_0, [], [])
